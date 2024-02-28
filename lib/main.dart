cd74e417824deef67425f0812b2df527bd11127d
Aaron <aaron_albertosatorres_@ceroca.cat>
2024-02-28 20:26:44 +0100

    Pagina registre

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_10/firebase_options.dart';
 import 'package:flutter_application_10/paginas/pagina_login.dart';
 
 void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MainApp());
 }
---
+++ lib/paginas/paina_registre.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_10/components/boto_auth.dart';
+import 'package:flutter_application_10/components/textfieid_auth.dart';
+
+class PAginaRegistre extends StatefulWidget {
+  const PAginaRegistre({super.key});
+
+  @override
+  State<PAginaRegistre> createState() => _PAginaRegistreState();
+}
+
+class _PAginaRegistreState extends State<PAginaRegistre> {
+   final TextEditingController controllerEmail = TextEditingController();
+  final TextEditingController controllerPassword = TextEditingController();
+  final TextEditingController controllerConfirmaPassword = TextEditingController();
+  void ferRegistre(){
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: const Color.fromARGB(255, 250, 183, 159),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+          
+                  //Logo.
+                  const Icon(
+                    Icons.fireplace,
+                    size: 120,
+                    color: Color.fromARGB(255, 255, 240, 218),
+                  ),
+            
+                  const SizedBox(height: 25,),
+            
+                  // Frase
+                  const Text(
+                    "Crea un compte nou",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 255, 240, 218),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+            
+                  const SizedBox(height: 25,),
+            
+                  // Text divisori.
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 25,),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            thickness: 1,
+                            color: Color.fromARGB(255, 255, 240, 218),
+                           ),
+                        ),
+                    
+                         Padding(
+                           padding: EdgeInsets.symmetric(horizontal: 4,),
+                           child: Text(
+                            "Fes Registre",
+                            style: TextStyle(color: Color.fromARGB(255, 255, 240, 218),),
+                           ),
+                         ),
+                         
+                         Expanded(
+                           child: Divider(
+                            thickness: 1,
+                            color: Color.fromARGB(255, 255, 240, 218),
+                           ),
+                         ),
+                      ],
+                    ),
+                  ),
+            
+                  const SizedBox(height: 10,),
+            
+                  // TextField Email.
+            
+                  TextFieldAuth(
+                    controller: controllerEmail, 
+                    hintText: "Email", 
+                    obscureText: false,
+                    ),
+            
+            
+                    const SizedBox(height: 10,),
+            
+                  // TextField Password.
+                  TextFieldAuth(
+                    controller: controllerPassword, 
+                    hintText: "Password", 
+                    obscureText: true,
+                    ),
+            
+                    const SizedBox(height: 10,),
+            
+            
+                  // No estas registrat/da?
+                     Padding(
+                      padding: const EdgeInsets.symmetric( horizontal: 25),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          const Text("Ya ets menbre?"),
+                          const SizedBox(width: 4,),
+                          GestureDetector(
+                            onTap: () {},
+                            child: const Text(
+                              "Fes login",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 10,),
+                  // Botó Registre
+            
+                  BotoAuth(
+                    text: "Registre",
+                    onTap: ferRegistre,
+                  ),
+              
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
