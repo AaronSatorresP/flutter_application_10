f0e571dd9d11c2b8ad1ae0050d93e6ac97317883
Aaron <aaron_albertosatorres_@ceroca.cat>
2024-04-03 20:24:05 +0200

    Preparant les imatges fins a android fet

--- lib/main.dart
+++ lib/main.dart
@@ -41,3 +41,26 @@ class MainApp extends StatelessWidget {
   -flutter pub add firebase_auth
   -flutter pub add firebase_core
 */
+
+/*
+imagenes:
+==========
+ 
+ 1) Habilitar Firebase Storage en el proyecto vinculat a Firabase.
+  - Es pot posar les reglas a true el wriete i read
+  2) Escarreguem dependencia de Firebase Storage al projecte
+  - flutter pub add firebase_storage
+  3) Descarraguem una dependencia per señeccionar imatge (un picket)
+  - N'hi ha diversos.
+  - imagen_picker
+    - flutter pub add imagen_picker
+ 
+ 
+  4) Perque funcioni en Android:
+    - Anar a android > app > src > main > AndroidManifest.xml
+    Escriure just abans del tag <aplication> (fora d'aplication):
+    <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/>
+    <uses-permission android:name="android.permission.READ_MEDIA_IMAGES"/>
+    <uses-permission android:name="android.permission.READ_MEDIA_VIDEO"/>
+
+*/
